575dc21310b1ae4b26b80bf41aa0f97ae261a9b7
Christopher Hittner <chrishittner@gmail.com>
2017-12-13 19:19:04 -0500

    Improved Main Driver
    I have added the ability to load source files for execution purposes.
    Currently, the program will simply print the output and then exit, assuming
    success. In the future, the exact specifics may change, especially if I
    add strings and printing.

--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,67 @@
 using namespace std;
 
 #include <iostream>
+#include <fstream>
 
-int main(int argc, char *argv[]) {
+void execute(string program) {
+    Value *v = run(program);
+    if (v) {
+        cout << *v << "\n";
+        delete v;
+    }
+}
+
+int interpret() {
     string program;
-
+    
     while (1) {
+        // Get input
         cout << "> ";
         getline(cin, program);
         
         if (program == "exit")
+            // Terminate the program
             return 0;
         else if (program != "") {
-            Value *v = run(program);
-            if (v)
-                cout << *v << "\n";
+            // Evaluate the program
+            execute(program);
         }
+    }
+}
+
+int main(int argc, char *argv[]) {
+    if (argc == 1)
+        // Run the interpreter
+        return interpret();
+    else if (argc == 2) {
+        // Attempt to open the file
+        ifstream file;
+        file.open(argv[1]);
+        
+        // Ensure that the file exists
+        if (!file) {
+            cerr << "\x1b[31m\x1b[1merror:\x1b[0m could not load program from '" << argv[1] << "'\n";
+            return 1;
+        }
+        
+        // Read the program from the input file
+        int i = 0;
+        string program = "";
+        do {
+            string s;
+            getline(file, s);
+
+            if (i++) program += "\n";
+            program += s;
+        } while (file);
+        
+        // Parse and execute the program.
+        execute(program);
+
+    } else {
+        // Throw an error
+        cerr << "usage: " << argv[0] << " [file | ]\n";
+        return 1;
     }
 }
 
