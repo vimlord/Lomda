ab37fc4ed5f988bd2036fdffd1d6b3128c480522
Christopher Hittner <chrishittner@gmail.com>
2018-04-22 19:21:24 -0400

    Migrated test cases to text files
    I have opted to do this in order to simplify test case editing.
    Now, I do not need to recompile in order to change test cases.
    Plus, the system is far more flexible, and could be modified to
    load an entire collection of test cases.

--- src/tests.cpp
+++ src/tests.cpp
@@ -1,82 +1,109 @@
 #include "tests.hpp"
 
+#include "bnf.hpp"
+#include "environment.hpp"
 #include "interp.hpp"
 
 #include <iostream>
+#include <fstream>
+
+#include <cstddef>
+#include <cstring>
+
+#include <map>
+#include <vector>
+
 using namespace std;
 
 #include <unistd.h>
 
+
 inline string txt_bold(string x) { return "\x1b[1m" + x + "\x1b[0m"; }
 
-int testcase(string x, string y) {
-    cout << "Case '" + x + "'...";
+/**
+ * Given a file, load a set of test cases
+ */
+std::map<Exp,string> load_test_cases(string fname) {
+    map<Exp,string> cases;
 
-    Val v = run(x);
-
-    cout << "\b\b\b ";
+    ifstream file;
+    file.open(fname);
     
-    if (!v) {
-        cout << "\x1b[31mfailed! (expected: " + y + ", threw error)\x1b[0m\n";
-        return 1;
-    } else if (v->toString() == y) {
-        cout << "\x1b[32mpassed!\x1b[0m\n";
-        v->rem_ref();
-        return 0;
-    } else {
-        cout << "\x1b[31mfailed! (expected: " + y + ", got: " + v->toString() + ")\x1b[0m\n";
-        v->rem_ref();
-        return 1;
+    if (!file) {
+        throw_err("", "could not load test cases from '" + fname + "'");
+    } else for (int i = 1; file; i++) {
+        string x, y;
+
+        // Get the input line
+        do { getline(file, x); } while (file && (x.length() == 0 || x[0] == '#'));
+        
+        if (!file) break;
+
+        // Get the output line
+        getline(file, y);
+        
+        Exp exp = compile(x);
+
+        if (exp) {
+            // Verify the legality of the case
+            Trie<bool> *vardta = new Trie<bool>;
+            bool valid = exp->postprocessor(vardta);
+            delete vardta;
+
+            // Keep the case if it passes the postprocessor
+            if (valid)
+                cases[exp] = y;
+            else
+                std::cout << "Test case " << i << " from '" << fname << "' will not be loaded (failed postprocessor)\n";
+        } else
+            // Discard it.
+            std::cout << "Test case " << i << " from '" << fname << "' will not be loaded (failed BNF parsing)\n";
     }
 
+    file.close();
+    std::cout << "Loaded " << cases.size() << " test cases from '" << fname << "'\n";
+
+    return cases;
 }
 
+int test_interp_cases(string title) {
+    int n = 0;
+    int i = 1;
+    auto interp_cases = load_test_cases("./tests/" + title + ".cases");
+
+    for (auto it : interp_cases) {
+        // Evaluate the test case
+        Exp exp = it.first;
+        Env env = new EmptyEnv;
+        Val v = exp->evaluate(env);
+        
+        // Garbage collection
+        delete exp;
+        env->rem_ref();
+
+        if (!v || v->toString() != it.second) {
+            std::cout << "Failed test case " << title << ":" << to_string(i) << "\n";
+            std::cout << "\tExpected " << it.second << ", got " << (v ? v->toString() : "null") << "\n";
+            n++;
+        }
+
+        // Destroy the value
+        v->rem_ref();
+
+        // Counter
+        i++;
+    }
+
+    return n;
+}
 
 int test() {
     int n = 0;
 
-    string tests[] = {
-        // Constant expressions
-        "2", "2",
-        "\"abc\"", "abc",
+    n += test_interp_cases("interp");
 
-        // Booleans
-        "true and false", "false",
-        "not false", "true",
-        
-        // Lambdas
-        "() -> 1", "λ.1 | {}",
-        "lambda (x, y, z) x * y + z", "λx,y,z.x * y + z | {}",
-        
-        // Let-exp
-        "let x = 2; x + 3", "5",
-        "let f(x) = x*x; f(3)", "9",
-        "let f() = 1; f()", "1",
-    
-        // Lists
-        "[1, 2, 3, 4][2]", "3",
-
-        // Dictionary
-        "{x:1,y:2}", "{x : 1, y : 2}",
-        "{}", "{}",
-        "let A = {x:1, y:2}; A[\"x\"] = A[\"y\"]; A", "{x : 2, y : 2}",
-        
-        // Loops
-        "let x = 0; for i in [1, 2, 3] x = x + i; x", "6",
-        "let x = 0, i = 1; while i <= 3 { x = x + i; i = i + 1 }; x", "6",
-
-        // Explicit typing
-        "\"123\" as int", "123",
-        "3.14 isa int", "false",
-        "[1, 2] isa list", "true",
-
-    ""};
-
-    for (int i = 0; tests[i] != ""; i += 2)
-        n += testcase(tests[i], tests[i+1]);
-
+    // Display end results
     std::cout << "\n";
-
     if (n)
         cout << "\x1b[31m" << "Failed " << n << " test cases\n";
     else
